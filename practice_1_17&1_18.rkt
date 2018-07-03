294227018ab622aad7de53c34e734a80d96fe1c9
AdorableNewYs <30628793+AdorableNewYs@users.noreply.github.com>
2018-07-03 14:58:45 +0800

    Update practice_1_17&1_18.rkt

--- practice_1_17&1_18.rkt
+++ practice_1_17&1_18.rkt
@@ -1,1 +1,22 @@
+;O(n)
+(define (* a b) 
+    (if (= b 0) 
+        0
+        (+ a (* a (- b 1)))))
 
+;;假定的过程:double 求出一个数的两倍、halve 求一个偶数的一半
+
+;O(log n)的递归计算过程
+(define (fast-* a b)
+    (cond ((= b 0) 0)
+          ((even? b) (fast-* (double a) (halve b))) 
+          (else (+ a (fast-* a (- b 1))))))
+
+;O(log n)的迭代计算过程
+(define (fast-* a b) 
+    (fast-*-iter a b 0))
+
+(define (fast-*-iter a b result) 
+    (cond ((= b 0) result)
+          ((even? b) (fast-*-iter (double a) (halve b) result))
+          (else (fast-*-iter a (- b 1) (+ a result)))))
