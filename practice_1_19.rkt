6d6d947a9ed5e13193f33e8155dbde6fd3e89f2a
AdorableNewYs <30628793+AdorableNewYs@users.noreply.github.com>
2018-07-03 15:11:47 +0800

    Create practice_1_19.rkt

---
+++ practice_1_19.rkt
@@ -0,0 +1,17 @@
+;O(log n)
+(define (fib n)
+    (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count) 
+    (cond ((= count 0) b) 
+          ((even? count) (fib-iter a
+                                   b 
+                                   (+ (* p p) (* q q)) 
+                                   (+ (* 2 p q) (* q q)) 
+                                   (halve count)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p)) 
+                          (+ (* b p) (* a q)) 
+                          p 
+                          q 
+                          (- count 1)))))
+
