0f32c79342824b95152f7180b686156ba9a2b283
AdorableNewYs <30628793+AdorableNewYs@users.noreply.github.com>
2018-07-19 10:56:00 +0800

    Create practice_1_23.rkt

---
+++ practice_1_23.rkt
@@ -0,0 +1,36 @@
+;;求和的高阶函数
+(define (sum term a next b) 
+    (if (> a b) 
+        0
+        (+ (term a) (sum term (next a) next b))))
+
+;;求函数f在a到b上的积分
+(define (integral f a b dx) 
+    (define (add-dx x) (+ x dx))
+    (* dx (sum f (+ a (/ dx 2)) add-dx b)))
+    
+;;用辛普森规则求积分    
+(define (Simpson-integral f a b n)
+    (define h (/ (+ a b) n))
+    (define (term x)
+        (f (+ a (* x h))))
+    (define (high-term y) 
+        (if (even? y) 
+            (* 2 (term y))
+            (* 4 (term y))))
+    (define (next z) 
+        (+ z 1))
+        
+        (* (/ h 3)
+           (+ (term 0) 
+              (term n) 
+              (sum high-term 1 next (- n 1)))))
+;;立方函数
+(define (cube x) (* x x x))
+
+;;结果
+;>  (Simpson-integral cube 0 1 10000)
+;1/4
+;> (integral cube 0 1 .00001)
+;0.24999999998662864
+;> 
